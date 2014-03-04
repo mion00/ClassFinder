ae3317bfcfe53ff0d0b5ba23b94210c57be13984
mion00 <mion00@gmail.com>
2014-03-04 21:41:01 +0100

    added HTML parser get id of departments

--- ClassFinder/src/main/Applicazione.java
+++ ClassFinder/src/main/Applicazione.java
@@ -9,6 +9,11 @@
 
 package main;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 
 
 /**
@@ -24,6 +29,20 @@ public class Applicazione {
     Applicazione() throws Exception
     {
         java.util.Date data=new java.util.Date();
+        
+        Document doc = Jsoup.connect("http://webapps.unitn.it/Orari/it/Web/CalendarioCds").get();
+        
+        Elements labels = doc.getElementsByAttributeValue("name", "id2");
+        
+        Elements dipartimenti = labels.get(0).getElementsByTag("option");
+        
+                
+        for (int i = 0; i<dipartimenti.size(); i++) {
+            System.out.println(dipartimenti.get(i).attributes() + " " + dipartimenti.get(i).html());
+//            TEST
+        }
+
+        
         DatiPolo dati=new DatiPolo("Povo1",data);
     }
 }
